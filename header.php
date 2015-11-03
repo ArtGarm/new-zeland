96766fa7aebc61bb02aa5464430e29638b30b066
trolizmaslom <trolizmaslom@gmail.com>
2015-11-03 16:27:16 +0200

    fix

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,700&subset=cyrillic' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700&subset=cyrillic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700&subset=cyrillic' rel='stylesheet' type='text/css'>
 	<link rel="shortcut icon" type="image/x-icon" href="images/shipfish.ico">
 
 	<title></title>
