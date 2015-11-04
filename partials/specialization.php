91776bfb19dde284079d54b20f4819c04ca19f38
Dmitry <dmitry1940@mail.ru>
2015-11-04 16:59:28 +0200

    specialization block

---
+++ partials/specialization.php
@@ -0,0 +1,48 @@
+<div class="specialization-main">
+    <div class="specialization">
+        <div class="mbox">
+
+            <div class="text-title-wrap white-color">
+                <div class="text-title">
+                    <span>ФУНКЦИОНАЛЬНЫЕ НАПРАВЛЕНИЯ</span>
+                </div>
+            </div>
+
+            <div class="specialization-wrap">
+                <ul>
+                    <li>
+                        <a href="#"><span>Легкая промышленность</span></a>
+                    </li>
+                    <li>
+                        <a href="#"><span>Строительство и недвижимость</span></a>
+                    </li>
+                    <li>
+                        <a href="#"><span>Логистика и транспорт</span></a>
+                    </li>
+                    <li>
+                        <a href="#"><span>Розничная торговля</span></a>
+                    </li>
+                    <li>
+                        <a href="#"><span>Государственный сектор</span></a>
+                    </li>
+                    <li>
+                        <a href="#"><span>Машиностроение</span></a>
+                    </li>
+                    <li>
+                        <a href="#"><span>Нефть, Газ, Химия</span></a>
+                    </li>
+                    <li>
+                        <a href="#"><span>Энергосбережение</span></a>
+                    </li>
+                    <li>
+                        <a href="#"><span>FMCG сектор</span></a>
+                    </li>
+                    <li>
+                        <a href="#"><span>IT-cистемы</span></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</div>
