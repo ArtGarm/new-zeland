bd33535e2d983d4e0a4db823688239f3aff5fe8d
Dmitry <dmitry1940@mail.ru>
2015-11-04 15:01:06 +0200

    functional-way

---
+++ partials/functionation.php
@@ -0,0 +1,106 @@
+<div class="functional-way functional-index">
+    <div class="mbox">
+
+        <div class="text-title-wrap">
+            <div class="text-title">
+                <span>ФУНКЦИОНАЛЬНЫЕ НАПРАВЛЕНИЯ</span>
+            </div>
+        </div>
+
+        <div class="functional-way-wrap">
+            <div class="functional-way-col">
+                <div class="functional-way-title">
+                    <span>СТРАТЕГИЧЕСКИЙ КОНСАЛТИНГ</span>
+                </div>
+                <div class="functional-way-list">
+                    <ul>
+                        <li>
+                            <a href="#">Многоуровневые стратегии развития</a>
+                        </li>
+                        <li>
+                            <a href="#">Стратегический маркетинг</a>
+                        </li>
+                        <li>
+                            <a href="#">Конкурентный анализ</a>
+                        </li>
+                        <li>
+                            <a href="#">Кадровый консалтинг</a>
+                        </li>
+                        <li>
+                            <a href="#">Бизнес планы, ТЭО</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="functional-way-col">
+                <div class="functional-way-title">
+                    <span>ЭКОНОМИЧЕСКИЕ ИССЛЕДОВАНИЯ</span>
+                </div>
+                <div class="functional-way-list">
+                    <ul>
+                        <li>
+                            <a href="#">Экономические и социологические исследования</a>
+                        </li>
+                        <li>
+                            <a href="#">Экономическая экспертиза и рекомендации</a>
+                        </li>
+                        <li>
+                            <a href="#">Долгосрочные прогнозы и программы</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="functional-way-col">
+                <div class="functional-way-title">
+                    <span>МЕЖДУНАРОДНЫЙ БИЗНЕС</span>
+                </div>
+                <div class="functional-way-list">
+                    <ul>
+                        <li>
+                            <a href="#">Проект менеджемент - управление зарубежными проектами</a>
+                        </li>
+                        <li>
+                            <a href="#">Инвестиционное сотрудничество</a>
+                        </li>
+                        <li>
+                            <a href="#">Содействие во внешнеторговой деятельности</a>
+                        </li>
+                        <li>
+                            <a href="#">Международные закупки (global sourcing)</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="functional-way-col">
+                <div class="functional-way-title">
+                    <span>ОПЕРАЦИОННЫЙ КОНСАЛТИНГ</span>
+                </div>
+                <div class="functional-way-list">
+                    <ul>
+                        <li>
+                            <a href="#">Планирование производства и сбыта</a>
+                        </li>
+                        <li>
+                            <a href="#">Товарно-материальные запасы</a>
+                        </li>
+                        <li>
+                            <a href="#">Операционная эффективность</a>
+                        </li>
+                        <li>
+                            <a href="#">Организационные изменения</a>
+                        </li>
+                        <li>
+                            <a href="#">Интеграция цепей поставок</a>
+                        </li>
+                        <li>
+                            <a href="#">Эффективное снабжение</a>
+                        </li>
+                        <li>
+                            <a href="#">Управленческие панели</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
