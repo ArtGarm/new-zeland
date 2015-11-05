5d1804330a4376d933a03aeb0c973fe884ea1c09
Radiksman <radion91@gmail.com>
2015-11-05 11:34:49 +0200

    add project

--- projects.php
+++ projects.php
@@ -1,1 +1,12 @@
 <?partial('title')?>
+<div class="projects">
+    <div class="mbox">
+        <div class="projects_wrap">
+            <div class="gray-title-wrap">
+                <div class="gray-title">
+                    <span>Наши наиболее крупные проекты</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
