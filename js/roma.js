24db833b3e60b2b5bdfae8b4b559c22254fd7572
Roma Kapustian <wff.kapustianroma@gmail.com>
2015-11-05 09:19:56 +0200

    no message

---
+++ js/roma.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$('.doneproject-slider').slick({
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  infinite: true
+	});
+});
--- partials/doneproject.php
+++ partials/doneproject.php
@@ -16,6 +16,10 @@
 							<div></div>
 							<div></div>
 						</div>
+						<div class="border-project"></div>
+						<a href="#">
+							<div class="hover-project"><p>Подробнее о проекте</p></div>
+						</a>
 					</div>
 					<div class="text-wrap">
 						<p>Развитие транспортного комплекса Республики Татарстан на 2011-2015 гг.</p>
@@ -27,6 +31,10 @@
 							<div></div>
 							<div></div>
 						</div>
+						<div class="border-project"></div>
+						<a href="#">
+							<div class="hover-project"><p>Подробнее о проекте</p></div>
+						</a>
 					</div>
 					<div class="text-wrap">
 						<p>Подготовка и защита заявок моногородов во Внешэкономбанке РФ</p>
@@ -38,6 +46,55 @@
 							<div></div>
 							<div></div>
 						</div>
+						<div class="border-project"></div>
+						<a href="#">
+							<div class="hover-project"><p>Подробнее о проекте</p></div>
+						</a>
+					</div>
+					<div class="text-wrap">
+						<p>Развитие рынка газомоторного топлива в Республике Татарстан на 2013-2023 годы</p>
+					</div>
+				</div>
+				<div class="doneproject">
+					<div class="doneproject-img">
+						<div class="image-wrap" style="background-image: url(images/project1.jpg);">
+							<div></div>
+							<div></div>
+						</div>
+						<div class="border-project"></div>
+						<a href="#">
+							<div class="hover-project"><p>Подробнее о проекте</p></div>
+						</a>
+					</div>
+					<div class="text-wrap">
+						<p>Развитие рынка газомоторного топлива в Республике Татарстан на 2013-2023 годы</p>
+					</div>
+				</div>
+				<div class="doneproject">
+					<div class="doneproject-img">
+						<div class="image-wrap" style="background-image: url(images/project1.jpg);">
+							<div></div>
+							<div></div>
+						</div>
+						<div class="border-project"></div>
+						<a href="#">
+							<div class="hover-project"><p>Подробнее о проекте</p></div>
+						</a>
+					</div>
+					<div class="text-wrap">
+						<p>Развитие рынка газомоторного топлива в Республике Татарстан на 2013-2023 годы</p>
+					</div>
+				</div>
+				<div class="doneproject">
+					<div class="doneproject-img">
+						<div class="image-wrap" style="background-image: url(images/project1.jpg);">
+							<div></div>
+							<div></div>
+						</div>
+						<div class="border-project"></div>
+						<a href="#">
+							<div class="hover-project"><p>Подробнее о проекте</p></div>
+						</a>
 					</div>
 					<div class="text-wrap">
 						<p>Развитие рынка газомоторного топлива в Республике Татарстан на 2013-2023 годы</p>
