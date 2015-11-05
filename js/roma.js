1b193d1c2f41c41f11d4598078e5235cdb2e0533
Roma Kapustian <wff.kapustianroma@gmail.com>
2015-11-05 11:28:35 +0200

    no message

--- js/roma.js
+++ js/roma.js
@@ -1,7 +1,37 @@
 $(document).ready(function(){
-	$('.doneproject-slider').slick({
-	  slidesToShow: 3,
-	  slidesToScroll: 1,
-	  infinite: true
 	});
+
+$(window).load(function(){
+	if ($(window).width() <= 768) {
+		$('.doneproject-slider').slick({
+		  slidesToShow: 1,
+		  slidesToScroll: 1,
+		  infinite: true
+		});
+	}
+	else{
+		$('.doneproject-slider').slick({
+		  slidesToShow: 3,
+		  slidesToScroll: 1,
+		  infinite: true
+		});
+	}
+});
+
+
+$(window).resize(function(){
+	if ($(window).width() <= 768) {
+		$('.doneproject-slider').slick({
+		  slidesToShow: 1,
+		  slidesToScroll: 1,
+		  infinite: true
+		});
+	}
+	else{
+		$('.doneproject-slider').slick({
+		  slidesToShow: 3,
+		  slidesToScroll: 1,
+		  infinite: true
+		});
+	}
 });
