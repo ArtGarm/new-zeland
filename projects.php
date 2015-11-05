00b09d8cd96e6a880b9914ad33b918c4ae09e436
Radiksman <radion91@gmail.com>
2015-11-05 11:41:33 +0200

    add ps

--- projects.php
+++ projects.php
@@ -7,6 +7,60 @@
                     <span>Наши наиболее крупные проекты</span>
                 </div>
             </div>
+            <div class="projects_item_wrap">
+                <div class="projects_item_row cfix">
+                    <div class="projects_items">
+                        <a href="#">
+                            <div class="projects_items_img">
+                                <div class="image-wrap" style="background-image: url(images/project1.jpg);">
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                                <div class="border-project"></div>
+                            </div>
+                            <div class="projects_items_text">
+                                <p>Развитие</p>
+                                <p>транспортного комплекса</p>
+                                <p>Республики Татарстан</p>
+                                <p>на 2011- 2015 гг. </p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="projects_items">
+                        <a href="#">
+                            <div class="projects_items_img">
+                                <div class="image-wrap" style="background-image: url(images/project1.jpg);">
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                                <div class="border-project"></div>
+                            </div>
+                            <div class="projects_items_text">
+                                <p>Подготовка и защита</p>
+                                <p>заявок моногородов</p>
+                                <p>во Внешэкономбанке РФ</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="projects_items">
+                        <a href="#">
+                            <div class="projects_items_img">
+                                <div class="image-wrap" style="background-image: url(images/project1.jpg);">
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                                <div class="border-project"></div>
+                            </div>
+                            <div class="projects_items_text">
+                                <p>Развитие рынка</p>
+                                <p>газомоторного топлива</p>
+                                <p>в Республике Татарстан</p>
+                                <p>на 2013-2023 годы</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
