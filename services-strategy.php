b6110fe02769ed4e5c22efccae6ce4dbd381df6a
trolizmaslom <trolizmaslom@gmail.com>
2015-11-05 09:31:27 +0200

    fix

--- services-strategy.php
+++ services-strategy.php
@@ -1,2 +1,46 @@
 <?partial('title')?>
+<div class="managment-wrapp services strategy">
+    <div class="mbox">
+        <div class="managment-align cfix">
+            <div class="menu-block">
+                <div class="menu-align">
+                    <div class="menu-title"><span>Услуги:</span></div>
+                    <div class="menu-item">
+                        <ul>
+                            <li><a href=""><span>Стратегический консалтинг</span></a></li>
+                            <li><a href=""><span>Экономические иследования</span></a></li>
+                            <li><a href=""><span>Международный бизнес</span></a></li>
+                            <li><a href=""><span>Операционный консалтинг</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="content-block">
+                <div class="content-align">
+                    <div class="content-item cfix">
+                        <div class="text-box">
+                            <div class="text-body">
+                                <p>SCM consult имеет большой практический опыт разработки стратегий, позволяющих достичь конкурентных преимуществ в изменении бизнес условий и внешних факторов путем конфигурации ресурсов и компетенций компании.</p>
+                            </div>
+                            <div class="text-title"><span>Многоуровневые стратегии развития:</span></div>
+                            <div class="text-body">
+                               <div class="ul">
+                                   <div class="li">
+                                       <p>Корпоративная стратегия - анализ целесообразности cуществующих корпоративных задач и возможность обеспечения корпоративным центром создания добавленной стоимости в подотчетных бизнес единицах.</p>
+                                   </div>
+                                   <div class="li">
+                                       <p>Стратегия бизнес единиц - анализ и разработка программ, позволяющих конкурировать более эффективно и обеспечивать устойчивые конкурентные преимущества.</p>
+                                   </div>
+                                   <div class="li">
+                                       <p>Функциональная стратегия - оценка и обеспечение участия различных функциональных служб в эффективном внедрении корпоративной и локальной бизнес стратегий.</p>
+                                   </div>
+                               </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?partial('foot-callback')?>
